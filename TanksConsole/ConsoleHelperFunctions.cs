5c7b07c0cceecc810c6f898339deeb1622ea0142
nickbelling <nickbelling@gmail.com>
2016-06-04 18:31:13 +1000

    Added a ConsoleHelperFunctions static class.

---
+++ TanksConsole/ConsoleHelperFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanksConsole
+{
+    public static class ConsoleHelperFunctions
+    {
+        public static int GetNumberFromUser()
+        {
+            int number = 0;
+            bool isValid = false;
+
+            while ( isValid == false )
+            {
+                Console.Write( "Please enter a number: " );
+                string input = Console.ReadLine();
+
+                if ( int.TryParse( input, out number ) )
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    Console.WriteLine( "That number was invalid, please try again." );
+                }
+            }
+
+            return number;
+        }
+    }
+}
--- TanksConsole/Program.cs
+++ TanksConsole/Program.cs
@@ -12,6 +12,11 @@ namespace TanksConsole
         static void Main(string[] args)
         {
             Tank tank1 = new Tank( "Tank 1" );
+
+            Console.WriteLine( "Please enter the number of tanks you want to create: " );
+            int numberOfTanks = ConsoleHelperFunctions.GetNumberFromUser();
+
+            Console.ReadKey();
         }
     }
 }
